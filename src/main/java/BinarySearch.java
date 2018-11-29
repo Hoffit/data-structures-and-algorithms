583d9c117332928937b29200cb15ec45ff4a854a
Hoffit <dehull_1@yahoo.com>
2018-11-28 19:05:25 -0800

    Initial setup for challenge

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+
+/**
+ * This is a CF 401 day 3 code challenge Java class.
+ */
+public class BinarySearch {
+
+    /**
+     * A method that can be used to search input sortedArray for value searchValue.
+     * @param sortedArray The input array to search. Must be sorted.
+     * @param searchKey The value to search for in sortedArray.
+     * @return The index in the sortedArray where searchKey is located.
+     */
+    protected int binarySearch(Array sortedArray, int searchKey) {
+        int result = -1;
+
+        return result;
+    }
+}
